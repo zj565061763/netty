b74ebf3817fc3d836eb873997fcd19b577da54a2
zjun <565061763@qq.com>
2026-07-04 01:10:31 +0800

    sample

--- app/src/main/java/com/sd/demo/netty/SampleViewModel.kt
+++ app/src/main/java/com/sd/demo/netty/SampleViewModel.kt
@@ -3,45 +3,49 @@ package com.sd.demo.netty
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sd.lib.netty.client.NettyClient
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
-import java.util.concurrent.ConcurrentHashMap
 
 class SampleViewModel : ViewModel() {
   private val _clients = MutableStateFlow<List<NettyClient>>(emptyList())
   val clients = _clients.asStateFlow()
 
-  private val _clientJobs = ConcurrentHashMap<NettyClient, Job>()
-
   fun addClient() {
     viewModelScope.launch {
-      try {
+      runCatching {
         val client = NettyClient(host = "127.0.0.1", port = App.server.port)
         client.connect()
         _clients.update { it + client }
-        _clientJobs[client] = client.connectionStateFlow.onEach { state ->
-          if (state == NettyClient.ConnectionState.DISCONNECTED) {
-            _clients.update { it - client }
-            _clientJobs.remove(client)?.cancel()
-          }
-        }.launchIn(viewModelScope)
-      } catch (e: Exception) {
-        e.printStackTrace()
-      }
+        initClient(client)
+      }.onFailure { logMsg { "client addClient error:${it.stackTraceToString()}" } }
     }
   }
 
   fun sendMessage(client: NettyClient, message: String) {
     viewModelScope.launch {
-      try {
-        client.send(message)
-      } catch (e: Exception) {
-        e.printStackTrace()
+      runCatching { client.send(message) }
+        .onFailure { logMsg { "client sendMessage error:${it.stackTraceToString()}" } }
+    }
+  }
+
+  private fun initClient(client: NettyClient) {
+    viewModelScope.launch {
+      val job = coroutineContext.job
+      launch {
+        client.connectionStateFlow.collect { state ->
+          if (state == NettyClient.ConnectionState.DISCONNECTED) {
+            _clients.update { it - client }
+            job.cancel()
+          }
+        }
+      }
+      launch {
+        client.messageFlow.collect { message ->
+          logMsg { "client message:$message" }
+        }
       }
     }
   }
