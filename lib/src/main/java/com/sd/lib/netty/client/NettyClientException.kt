ac448e13724c40e35c4326c6ef0e51116c9e8e2d
zjun <565061763@qq.com>
2026-07-04 15:53:34 +0800

    comment

--- lib/src/main/java/com/sd/lib/netty/client/NettyClientException.kt
+++ lib/src/main/java/com/sd/lib/netty/client/NettyClientException.kt
@@ -2,7 +2,7 @@ package com.sd.lib.netty.client
 
 open class NettyClientException internal constructor(message: String? = null, cause: Throwable? = null) : Exception(message, cause)
 
-/** 还未准备好 */
+/** 还未就绪 */
 class NettyClientNotReadyException internal constructor() : NettyClientException()
 
 /** 超时异常 */
--- lib/src/main/java/com/sd/lib/netty/server/NettyServerException.kt
+++ lib/src/main/java/com/sd/lib/netty/server/NettyServerException.kt
@@ -2,11 +2,11 @@ package com.sd.lib.netty.server
 
 open class NettyServerException internal constructor(message: String? = null, cause: Throwable? = null) : Exception(message, cause)
 
-/** 客户端未就绪或不处于激活状态 */
-class NettyServerClientNotReadyException internal constructor() : NettyServerException()
-
 /** 客户端不存在 */
 class NettyServerClientNotFoundException internal constructor() : NettyServerException()
+
+/** 客户端未就绪 */
+class NettyServerClientNotReadyException internal constructor() : NettyServerException()
 
 /** 超时异常 */
 class NettyServerTimeoutException internal constructor() : NettyServerException()
